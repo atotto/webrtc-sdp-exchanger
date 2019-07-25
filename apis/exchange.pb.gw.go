01ca8a9f282481b4244766c5b3b24a206b95dad2
Ato Araki <ato.araki@gmail.com>
2019-07-25 23:06:28 +0900

    add delete session api

--- apis/exchange.pb.gw.go
+++ apis/exchange.pb.gw.go
@@ -117,6 +117,33 @@ func request_ExchangeService_GetSessionOffer_0(ctx context.Context, marshaler ru
 
 }
 
+func request_ExchangeService_DeleteSession_0(ctx context.Context, marshaler runtime.Marshaler, client ExchangeServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq DeleteSessionRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["session_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "session_id")
+	}
+
+	protoReq.SessionId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "session_id", err)
+	}
+
+	msg, err := client.DeleteSession(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterExchangeServiceHandlerFromEndpoint is same as RegisterExchangeServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterExchangeServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -215,6 +242,26 @@ func RegisterExchangeServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 
 	})
 
+	mux.Handle("DELETE", pattern_ExchangeService_DeleteSession_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_ExchangeService_DeleteSession_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_ExchangeService_DeleteSession_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -224,6 +271,8 @@ var (
 	pattern_ExchangeService_GetSessionAnswer_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2}, []string{"sessions", "session_id", "answer"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_ExchangeService_GetSessionOffer_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2}, []string{"sessions", "session_id", "offer"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_ExchangeService_DeleteSession_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1}, []string{"sessions", "session_id"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -232,4 +281,6 @@ var (
 	forward_ExchangeService_GetSessionAnswer_0 = runtime.ForwardResponseMessage
 
 	forward_ExchangeService_GetSessionOffer_0 = runtime.ForwardResponseMessage
+
+	forward_ExchangeService_DeleteSession_0 = runtime.ForwardResponseMessage
 )
